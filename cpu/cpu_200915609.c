756c86c5834df95baff401847fa273975b8d1819
Gary Ortiz <goritz1490@gmail.com>
2020-09-16 10:31:17 -0600

    Archivos iniciales

---
+++ cpu/cpu_200915609.c
@@ -0,0 +1,84 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <asm/uaccess.h>
+#include <linux/sched/signal.h>
+#include <linux/sched.h>
+ 
+
+#define PROCFS_NAME "cpu_200915609"
+
+
+struct task_struct *task;          
+struct task_struct *task_child;    
+struct list_head *list;  
+
+static int cpu_show(struct seq_file *m, void *v){
+    seq_printf(m, "Carne: 200915609 \n");
+    seq_printf(m, "Nombre: Gary Joan Ortiz Lopez \n");
+    seq_printf(m, "SO: Debian  \n");
+
+    int numeroProceso = 0;
+    struct task_struct *task;
+    for_each_process(task){
+        seq_printf(m, "------------%d----------\n", numeroProceso);
+        seq_printf(m, "PID: %d \n", task->pid);
+        seq_printf(m, "Nombre: %s \n", task->comm);
+        seq_printf(m, "Estado: ");
+        switch(task->state){
+            case 0:
+                seq_printf(m, "Ejecutando\n");
+                break;
+            case 1:
+                seq_printf(m, "Listo\n");
+                break;
+            case 2:
+                seq_printf(m, "Durmiendo\n");
+                break;
+            case 4:
+                seq_printf(m, "Zombie\n");
+                break;
+            case 8:
+                seq_printf(m, "Detenido\n");
+                break;
+            case 32:
+                seq_printf(m, "En Espera\n");
+                break;
+        }
+    }
+    return 0;
+}
+
+static int cpu_open(struct inode *inode, struct file *file){
+return single_open(file, cpu_show, NULL);
+}
+
+static const struct file_operations cpu_fops = {
+    .owner = THIS_MODULE,
+    .open = cpu_open,
+    .read = seq_read,
+    .llseek = seq_lseek,
+    .release = single_release,
+};
+
+static int ver_cpu_init(void){
+	printk(KERN_INFO "Cargando modulo cpu.\r\n");
+	proc_create(PROCFS_NAME, 0, NULL, &cpu_fops);
+	printk(KERN_INFO "Nombre : Gary Joan Ortiz Lopez \n Carnet : 200915609 \n Completado. Procceso: /proc/%s.\r\n", PROCFS_NAME);
+	return 0;
+}
+
+static void ver_cpu_exit(void){
+        
+        printk(KERN_INFO "Modulo CPU Deshabilitado.\r\n");
+        remove_proc_entry(PROCFS_NAME, NULL);		        
+}
+
+module_init(ver_cpu_init);
+module_exit(ver_cpu_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("HB");
+MODULE_DESCRIPTION("ejemplo de como menejar cpu");
---
+++ memoria/memoria_200915609.c
@@ -0,0 +1,88 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <asm/uaccess.h>
+/* to get works meminfo */
+#include <linux/hugetlb.h>
+#include <linux/mm.h>
+#include <linux/mman.h>
+#include <linux/mmzone.h>
+#include <linux/quicklist.h>
+#include <linux/swap.h>
+#include <linux/vmstat.h>
+#include <linux/atomic.h>
+#include <asm/page.h>
+#include <asm/pgtable.h>
+
+#define PROCFS_NAME "memoria_200915609"
+
+struct sysinfo i;
+unsigned long committed;
+unsigned long allowed;
+//struct vmalloc_info vmi;
+long cached;
+unsigned long pages[NR_LRU_LISTS];
+int lru;
+
+static int memori_show(struct seq_file *m, void *v){
+
+int porcentaje = 0;
+seq_printf(m,"Nombre: Gary Joan Ortiz Lopez\n");
+seq_printf(m,"Carnet: 200915609\n");
+seq_printf(m, "SO: Debian\n");
+seq_printf(m, "Proceso:\n");
+seq_printf(m, "\n");
+
+#define K(x) ((x) << (PAGE_SHIFT - 10))
+si_meminfo(&i); 
+
+porcentaje = (i.freeram*100)/i.totalram;
+seq_printf(m,"MemTotal: %8lu kB\n",K(i.totalram));
+seq_printf(m,"MemFree: %8lu kB\n",K(i.freeram));
+seq_printf(m,"Buffers: %8lu kB\n",K(i.bufferram));
+seq_printf(m, "Porcentaje Libre: %8lu\n",porcentaje);
+
+#ifdef CONFIG_HIGHMEM
+seq_printf(m,"HighTotal: %8lu kB\n",K(i.totalhigh));
+seq_printf(m,"HighFree: %8lu kB\n",K(i.freehigh));
+seq_printf(m,"LowTotal: %8lu kB\n",K(i.totalram-i.totalhigh));
+seq_printf(m,"LowFree: %8lu kB\n",K(i.freeram-i.freehigh));
+#endif
+
+
+#undef K
+return 0;
+}
+
+static int memori_open(struct inode *inode, struct file *file){
+return single_open(file, memori_show, NULL);
+}
+
+static const struct file_operations memori_fops = {
+.owner = THIS_MODULE,
+.open = memori_open,
+.read = seq_read,
+.llseek = seq_lseek,
+.release = single_release,
+};
+
+static int __init memori_init(void){
+printk(KERN_INFO "Cargando modulo memoria_200915609.\r\n");
+proc_create(PROCFS_NAME, 0, NULL, &memori_fops);
+printk(KERN_INFO "Nombre : Gary Joan Ortiz Lopez \n Carnet : 200915609 \n Completado. Procceso: /proc/%s.\r\n", PROCFS_NAME);
+return 0;
+}
+
+static void __exit memori_exit(void){
+
+printk(KERN_INFO "Modulo memoria Deshabilitado.\r\n");
+remove_proc_entry(PROCFS_NAME, NULL);
+}
+
+module_init(memori_init);
+module_exit(memori_exit);
+
+MODULE_LICENSE("GPL");
