223caf79a9d77328018fe04f90bcc9fe5f97d18b
Gary Ortiz <goritz1490@gmail.com>
2020-09-17 18:47:26 -0600

    servidor flask y mongodb funcionando

--- app.py
+++ app.py
@@ -1,38 +1,26 @@
 import os
 from flask import Flask, redirect, url_for, request, render_template
 from pymongo import MongoClient
-
 app = Flask(__name__)
-
 client = MongoClient(
     os.environ['DB_PORT_27017_TCP_ADDR'],
-    27017)
+    27017,username='root',password='rootpassword')
 db = client.tododb
-
 @app.route('/hello')
-def todo():
-
-    return 'Hola mundo!!'
+def hello():
+    return 'Hola Mundo'
 @app.route('/')
 def todo():
-
     _items = db.tododb.find()
     items = [item for item in _items]
-
-    return render_template('index.html', items=items)
-
-
+    return render_template('todo.html', items=items)
 @app.route('/new', methods=['POST'])
 def new():
-
     item_doc = {
         'name': request.form['name'],
         'description': request.form['description']
     }
     db.tododb.insert_one(item_doc)
-
     return redirect(url_for('todo'))
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0',port=80, debug=True)
