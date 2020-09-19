8f8a3a3e9fe050340304eed61e15a77f1ff0f5cc
Gary Ortiz <goritz1490@gmail.com>
2020-09-19 17:53:13 -0600

    fixing json module

--- cpu/cpu_200915609.c
+++ cpu/cpu_200915609.c
@@ -8,7 +8,7 @@
 #include <linux/sched/signal.h>
 #include <linux/sched.h>
  
-
+static int hz=100;
 #define PROCFS_NAME "cpu_200915609"
 
 
@@ -17,52 +17,29 @@ struct task_struct *task_child;
 struct list_head *list;  
 
 static int cpu_show(struct seq_file *m, void *v){
-    seq_printf(m, "Carne: 200915609 \n");
-    seq_printf(m, "Nombre: Gary Joan Ortiz Lopez \n");
     int numeroProceso = 0;
     int total_time = 0;
     int seconds = 0; 
     int cpu_usage = 0;
     int cpu_total;
+    seq_printf(m, "{ \"CPU\": {");
     for_each_process(task){
-        total_time = task->utime + task->stime;
-        
-        seconds = task->utime - (task->start_time / hz);
-        cpu_usage = 100 * ((total_time/ hz)/seconds);
+        total_time = total_time + task->utime + task->stime;       
+        seconds = seconds +( task->utime - (task->start_time / hz));
         list_for_each(list, &task->children){                        
  
             task_child = list_entry( list, struct task_struct, sibling );    
      
             total_time = total_time + task_child->utime +task_child->stime;
-        } 
-        seq_printf(m, "------------%d----------\n", numeroProceso);
-        seq_printf(m, "HZ : %d \n", hz);
-        seq_printf(m, "total time : %d \n", total_time);
-        seq_printf(m, "seconds : %d \n", seconds);
-        seq_printf(m, "CPU TIME : %d \n", cpu_usage);
-        seq_printf(m, "Nombre: %s \n", task->comm);
-        seq_printf(m, "Estado: ");
-        switch(task->state){
-            case 0:
-                seq_printf(m, "Ejecutando\n");
-                break;
-            case 1:
-                seq_printf(m, "Listo\n");
-                break;
-            case 2:
-                seq_printf(m, "Durmiendo\n");
-                break;
-            case 4:
-                seq_printf(m, "Zombie\n");
-                break;
-            case 8:
-                seq_printf(m, "Detenido\n");
-                break;
-            case 32:
-                seq_printf(m, "En Espera\n");
-                break;
-        }
+                 }
+        
     }
+    seq_printf(m, "{\n");
+    seq_printf(m, "\"HZ\" : %d ,\n", hz);
+    seq_printf(m, "\"total time\" : %d ,\n", total_time);
+    seq_printf(m, "\"seconds \": %d \n", seconds);
+    seq_printf(m, "}\n");
+    seq_printf(m, " }");
     return 0;
 }
 
