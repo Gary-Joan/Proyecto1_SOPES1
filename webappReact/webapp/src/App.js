06f72b040e5ed0b402af8d88fa0c508421f488d7
Gary Ortiz <goritz1490@gmail.com>
2020-09-28 17:31:50 -0600

    prueba practica8 sa

--- webappReact/webapp/src/App.js
+++ webappReact/webapp/src/App.js
@@ -3,16 +3,28 @@ import './App.css';
 import Nav from './components/nav';
 import Publicaciones from './components/publicaciones';
 import Graficas from './components/graficas';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-     <Nav />
-     <Publicaciones />
-     <Graficas />
-
-    </div>
+    
+      <Router >
+          <div className="App">
+          <Nav />
+          <Switch>
+          <Route path="/" exact component={home}/>
+          <Route path="/publi" component={Publicaciones} />
+          <Route path="/grafica" component={Graficas} />
+          </Switch>
+      </div>
+     </Router>
+   
+   
   );
 }
-
+ const home = () => (
+   <div>
+     <h1>Pagina principal</h1>
+   </div>
+ );
 export default App;
--- webappReact/webapp/src/components/nav.js
+++ webappReact/webapp/src/components/nav.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import '../App.css'
-
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
 function Nav() {
-    return ( 
-        <nav>
-            <h4>SOPES 1</h4>
-            <ul className="nav-links">
-                
-                <li>Publicaciones</li>
-                <li>Graficas</li>
-            </ul>
-        </nav>
-        );
+  const navstyle = {
+    color: "white",
+  };
+  return (
+    <nav>
+      <h4>SOPES 1</h4>
+      <ul className="nav-links">
+        <Link style={navstyle} to="/publi">
+          <li>Publicaciones</li>
+        </Link>
+        <Link style={navstyle} to="/grafica">
+          <li>Graficas</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
